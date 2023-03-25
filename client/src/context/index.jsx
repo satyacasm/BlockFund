2b9262f4a0ec20729941136de64b2afb6813202d
satyacasm <202151141@iiitvadodara.ac.in>
2023-03-26 03:04:11 +0530

    Final Touches 💫💫

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x595a8b16d29e6D9Ec96D800a1fDa439e760BC04F');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
