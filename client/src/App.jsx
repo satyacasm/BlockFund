859a0226be1e8027abf8cc2df48bb85b5e057038
Satyam Tripathi <98680993+satyacasm@users.noreply.github.com>
2023-03-26 01:29:33 +0530

    Revert ""change in client""

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom';
+import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
 
-import {Home,Profile,CampignDetail,CreateCampaign} from './pages'
-import {Navbar,Sidebar} from './components'
-const App = () => {
+export default function Home() {
   return (
-    <div className='relative sm:-8 p-4 gradient-bg-welcome'>
-     app
-  <Navbar/>
-  <Sidebar/>
-     <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-campaign" element={<CreateCampaign />} />
-          <Route path="/campaign-details/:id" element={<CampignDetail />} />
-        </Routes>
-    </div>
-  )
-}
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+        </h1>
 
-export default App
+        <p className="description">
+          Get started by configuring your desired network in{" "}
+          <code className="code">src/main.jsx</code>, then modify the{" "}
+          <code className="code">src/App.jsx</code> file!
+        </p>
+
+        <div className="connect">
+          <ConnectWallet />
+        </div>
+
+        <div className="grid">
+          <a href="https://portal.thirdweb.com/" className="card">
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className="card">
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a href="https://portal.thirdweb.com/templates" className="card">
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
